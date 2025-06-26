3004767959e8811e61e00207b3fa8c1e084314cf
Ícaro Pegoraro <icaropegsakurai@gmail.com>
2025-06-25 21:37:46 -0300

    Adicionando Grids no FormRepresentante

--- src/pages/empresas/cadastro/Form/FormRepresentanteEmpresa/FormRepresentante.jsx
+++ src/pages/empresas/cadastro/Form/FormRepresentanteEmpresa/FormRepresentante.jsx
@@ -1,13 +1,14 @@
-import { Box, Typography } from "@mui/material"
+import { Grid, Typography } from "@mui/material"
 import { FormDadosRepresentante } from "./components/FormDadosRepresentante"
 import { FormEnderecoRepresentante } from "./components/FormEnderecoRepresentante"
 
 export const FormRepresentante = () => {
     return (
-        <Box sx={{ mb: 3 }}>
-            <Typography variant="h5" gutterBottom>Dados da Empresa</Typography>
+        <Grid container spacing={2}>
+            <Typography variant="h5" gutterBottom>Dados do representante</Typography>
             <FormDadosRepresentante />
+            <Typography variant="h5" gutterBottom>Endereço do representante</Typography>
             <FormEnderecoRepresentante />
-        </Box>
+        </Grid>
     )
 }
--- src/pages/empresas/cadastro/Form/FormRepresentanteEmpresa/components/FormDadosRepresentante.jsx
+++ src/pages/empresas/cadastro/Form/FormRepresentanteEmpresa/components/FormDadosRepresentante.jsx
@@ -6,79 +6,87 @@ export const FormDadosRepresentante = () => {
   const { register } = useFormContext()
 
   return (
-    <Box>
-      <Grid>
+    <Grid container spacing={2}>
+      <Grid size={{ xs: 12, md: 3 }}>
         <TextField
+          fullWidth
           label="CPF"
           name="cpf"
           {...register("representante.cpf")}
         />
       </Grid>
 
-      <Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
         <TextField
+          fullWidth
           label="Nome"
           name="nome"
           {...register("representante.nome")}
         />
       </Grid>
 
-      <Grid>
+      <Grid size={{ xs: 12, md: 3 }}>
         <TextField
+          fullWidth
           label="Nome social"
           name="nomeSocial"
           {...register("representante.nomeSocial")}
         />
       </Grid>
 
-      <Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
         <TextField
+          fullWidth
           label="RG"
           name="rg"
           {...register("representante.rg")}
         />
       </Grid>
 
-      <Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
         <TextField
+          fullWidth
           label="UF de emissão"
           name="ufEmissao"
           {...register("representante.ufEmissao")}
         />
       </Grid>
 
-      <Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
         <TextField
+          fullWidth
           label="Telefone"
           name="telefone"
           {...register("representante.telefone")}
         />
       </Grid>
 
-      <Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
         <TextField
+          fullWidth
           label="Nome da mãe"
           name="nomeMaeRepresentante"
           {...register("representante.nomeMaeRepresentante")}
         />
       </Grid>
 
-      <Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
         <TextField
+          fullWidth
           label="Email"
           name="email"
           {...register("representante.email")}
         />
       </Grid>
 
-      <Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
         <TextField
+          fullWidth
           label="Data de nascimento"
           name="dataNascimento"
           {...register("representante.dataNascimento")}
         />
       </Grid>
-
-    </Box>
-  );
-};
+    </Grid>
+  )
+}
